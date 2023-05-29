23180ed161f3b4aaa5b53873f18e95e82fc1e4fe
gabrielMulti <gabriel.barbosa@multicoisas.com.br>
2023-05-29 08:21:46 -0400

    v1.0.2

---
+++ index.js
@@ -0,0 +1,9 @@
+const Transection = require('./models/transaction')
+const Job = require('./models/job')
+const Trace = require('./models/trace')
+
+module.exports = {
+    Trace,
+    Job,
+    Transection
+}
--- prisma/index.js
+++ prisma/index.js
@@ -2,5 +2,5 @@ const { execSync } = require("child_process")
 const { join } = require("path")
 
 module.exports = function generateSchema() {
-    execSync(`npx prisma generate --schema=${join(process.cwd() + '/schema.prisma')}`, { cwd: join(__dirname + "/../"), shell: true, stdio: "inherit" })
+    execSync(`npx prisma generate`, { cwd: join(__dirname + "/../"), shell: true, stdio: "inherit" })
 }
